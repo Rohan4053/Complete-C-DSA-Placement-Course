0b446fd180c12ca53baf7bdb5f94fd8badfade65
Rohan4053 <144653383+Rohan4053@users.noreply.github.com>
2025-03-14 21:49:55 +0000

    Commited

--- Patterns/Triangle.cpp
+++ Patterns/Triangle.cpp
@@ -1,20 +1,21 @@
 #include<iostream>
 using namespace std;
+
+/*
+*
+* *
+* * *
+* * * *
+* * * * *
+*/
 int main(){
     int n;
     cin>>n;
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            cout<<"* ";
-        }
-        cout<<endl;
-    }
 
-    //Through while loops
     int i=1;
     while(i<=n){
         int j=1;
-        while(j<=n){
+        while(j<=i){
             cout<<"* ";
             j++;
         }
